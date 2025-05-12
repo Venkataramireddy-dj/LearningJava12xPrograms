7e9c4f575ede8b104d3950f60399ee775368db08
Venkataramireddy <venkataramireddych09@gmail.com>
2025-05-12 10:07:38 +0530

    This is printf assignment on 11/.05

---
+++ src/ex_01_JavaBasics/excercise_Of_Printf.java
@@ -0,0 +1,21 @@
+package ex_01_JavaBasics;
+
+public class excercise_Of_Printf {
+        public static void main(String[] args) {
+            int num =5;
+            System.out.printf("%dx1=%d\n",num,num*1);
+            System.out.printf("%dx1=%d\n",num,num*2);
+            System.out.printf("%dx1=%d\n",num,num*3);
+            System.out.printf("%dx1=%d\n",num,num*4);
+            System.out.printf("%dx1=%d\n",num,num*5);
+            System.out.printf("%dx1=%d\n",num,num*6);
+            System.out.printf("%dx1=%d\n",num,num*7);
+            System.out.printf("%dx1=%d\n",num,num*8);
+            System.out.printf("%dx1=%d\n",num,num*9);
+            System.out.printf("%dx1=%d\n",num,num*10);
+
+
+        }
+    }
+
+
