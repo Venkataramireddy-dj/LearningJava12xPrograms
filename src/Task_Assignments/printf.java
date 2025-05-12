51566a3dbe188ed15834f08e90542536f835606c
Venkataramireddy <venkataramireddych09@gmail.com>
2025-05-12 09:57:39 +0530

    This is printf assignment on 11/.05

---
+++ src/Task_Assignments/printf.java
@@ -0,0 +1,19 @@
+package Task_Assignments;
+
+public class printf {
+    public static void main(String[] args) {
+        int num =5;
+        System.out.printf("%dx1=%d\n",num,num*1);
+        System.out.printf("%dx1=%d\n",num,num*2);
+        System.out.printf("%dx1=%d\n",num,num*3);
+        System.out.printf("%dx1=%d\n",num,num*4);
+        System.out.printf("%dx1=%d\n",num,num*5);
+        System.out.printf("%dx1=%d\n",num,num*6);
+        System.out.printf("%dx1=%d\n",num,num*7);
+        System.out.printf("%dx1=%d\n",num,num*8);
+        System.out.printf("%dx1=%d\n",num,num*9);
+        System.out.printf("%dx1=%d\n",num,num*10);
+
+
+    }
+}
