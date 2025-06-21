92fe7f60a23ed4d2a81b570edd6ed80018a704c3
Venkataramireddy <venkataramireddych09@gmail.com>
2025-06-21 12:39:25 +0530

    This is 18th june for loop task

---
+++ src/For_Loop_Examples/Print_Hello.java
@@ -0,0 +1,9 @@
+package For_Loop_Examples;
+
+public class Print_Hello {
+    public static void main(String[] args) {
+        for(int i = 0; i<=4; i++){
+            System.out.println("Hello");
+        }
+    }
+}
---
+++ src/For_Loop_Examples/Sum_Of_Natural_numbers.java
@@ -0,0 +1,12 @@
+package For_Loop_Examples;
+
+public class Sum_Of_Natural_numbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for(int i = 1; i<=10; i++){
+            sum+=i;
+        }
+        System.out.println("Sum of first 10 natural numbers are " + sum );
+
+    }
+}
