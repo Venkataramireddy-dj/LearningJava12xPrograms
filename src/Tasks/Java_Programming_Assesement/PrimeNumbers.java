833a2fbdd99b3f2e937829b55d2c669e5c12fd5c
Venkataramireddy <venkataramireddych09@gmail.com>
2025-06-30 15:20:06 +0530

    This is 28th june task of Prime

---
+++ src/Tasks/Java_Programming_Assesement/PrimeNumbers.java
@@ -0,0 +1,18 @@
+package Tasks.Java_Programming_Assesement;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        for(int num =2; num<=100; num++){
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break; // Exit inner loop if factor found
+                }
+            }
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
