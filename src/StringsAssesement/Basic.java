fe569414d2ed27958896da89910fec024dd807d7
Venkataramireddy <venkataramireddych09@gmail.com>
2025-06-30 16:06:20 +0530

    This is 28th june task of String basic

---
+++ src/StringsAssesement/Basic.java
@@ -0,0 +1,13 @@
+package StringsAssesement;
+
+public class Basic {
+    public static void main(String[] args) {
+        String word = "Hello world";
+        System.out.println(word + "123");
+        System.out.println(" Concatination : " + word + "123");
+        System.out.println(" Length : " + word.length());
+        System.out.println(" Substring : " + word.substring(6));
+        System.out.println(" Character extraction : " + word.charAt(0));
+
+    }
+}
